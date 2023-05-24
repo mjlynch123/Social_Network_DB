688cbeeddaa1b0e441948924ed0127ba1d6d5914
M4RT1N <mjlynch35@yahoo.com>
2023-05-24 07:03:53 -0500

    Got server up and running

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const router = require("express").router();
+const router = require("express").Router();
 const apiRoutes = require("./api");
 
 router.use('/api', apiRoutes);
 
-router.user((req, res) => {
+router.use((req, res) => {
     return res.send('Wrong Route!');
 });
 
